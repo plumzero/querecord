c405aa674c7ecbe5a01b3f4ae016c9e35f64f1cb
libeio <ytmrlimei@163.com>
2021-07-22 00:03:06 +0800

    ResponseWriter part

---
+++ 04_Go/ch08/t/02_WriteHeader.go
@@ -0,0 +1,21 @@
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func noAuth(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(401)
+	fmt.Fprintf(w, "未授权，认证后才能访问该接口!")
+}
+
+func main() {
+	http.HandleFunc("/noAuth", noAuth)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
