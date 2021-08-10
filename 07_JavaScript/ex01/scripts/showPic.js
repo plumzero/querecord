7ee4c953269b48ab7ec7931972a54d6932d54cc3
libei <ytmrlimei@163.com>
2021-08-10 19:42:39 +0800

    js dom

--- 07_JavaScript/ex01/scripts/showPic.js
+++ 07_JavaScript/ex01/scripts/showPic.js
@@ -1,11 +1,19 @@
 
+// 你要自己决定是否需要这么多的判断
 function showPic(whichPic) {
+    if (! document.getElementById("placeholder")) return false;
     var source = whichPic.getAttribute("href");
     var placeholder = document.getElementById("placeholder");
     placeholder.setAttribute("src", source);
-    var text = whichPic.getAttribute("title");
-    var description = document.getElementById("description");
-    description.firstChild.nodeValue = text;
+    // 如果没有 description 就算了
+    if (document.getElementById("description")) {
+        var text = whichPic.getAttribute("title") ? whichPic.getAttribute("title") : "";
+        var description = document.getElementById("description");
+        if (description.firstChild.nodeType == 3) {
+            description.firstChild.nodeValue = text;
+        }
+    }
+    return true;
 }
 
 function countBodyChildren() {
@@ -14,7 +22,6 @@ function countBodyChildren() {
 }
 
 function prepareGallery() {
-    alert("=========================");
     // 对象检测
     if (! document.getElementsByTagName) return false;
     if (! document.getElementById) return false;
@@ -24,12 +31,13 @@ function prepareGallery() {
     var links = gallery.getElementsByTagName("a");
     for (var i = 0; i < links.length; i++) {
         links[i].onclick = function() {
-            showPic(this);
-            return false;
+            return ! showPic(this);     // 如果返回 false，则图片未更新，返回 true 允许默认行为发生。
         }
+        links[i].onkeypress = links[i].onclick; // 允许键盘事件。不过，最好不要使用 onkeypress 事件处理函数
     }
 }
 
+// 利用 addLoadEvent 可以加载多个预处理函数执行，而仅使用 window.onload 的话只能执行最后一个
 function addLoadEvent(func) {
     var oldonload = window.onload;
     if (typeof window.onload != 'function') {
