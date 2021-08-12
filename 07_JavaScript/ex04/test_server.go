fdbf02713fbe975f1fceed4846dadf2636caa97c
libei <ytmrlimei@163.com>
2021-08-12 20:36:53 +0800

    test server for ajax request

--- 07_JavaScript/ex04/test_server.go
+++ 07_JavaScript/ex04/test_server.go
@@ -3,14 +3,14 @@ package main
 import (
     "net/http"
     "github.com/gin-gonic/gin"
-    "fmt"
 )
 
 func PostCallback(c *gin.Context) {
-
     buf := make([]byte, 1024)
     n, _ := c.Request.Body.Read(buf)
-    fmt.Println(string(buf[0:n]))
+
+    c.Header("Access-Control-Allow-Origin",      "*")
+    c.Header("Access-Control-Allow-Headers",     "Origin, X-Requested-With, Content-Type, Accept")
 
     c.JSON(200, gin.H{
         "Post": string(buf[0:n]),
@@ -22,12 +22,12 @@ func PostCallback(c *gin.Context) {
 func main() {
     router := gin.Default()
 
-    // curl http://127.0.0.1:8080/api/v1/red
     router.GET("/api/v1/red", func(c *gin.Context) {
+        c.Header("Access-Control-Allow-Origin",      "*")
+        c.Header("Access-Control-Allow-Headers",     "Origin, X-Requested-With, Content-Type, Accept")
         c.String(http.StatusOK, "Get is red")
     })
 
-    // curl -X POST http://127.0.0.1:8080/api/v1/blue -d'{"username" : "ultraboy", "userage" : "20394"}'
     router.POST("api/v1/blue", PostCallback)
 
     router.Run(":8080")
