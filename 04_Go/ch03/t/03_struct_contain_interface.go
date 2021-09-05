3b6525b93470e7623c435a53625b6a24b0fae26f
libeio <ytmrlimei@163.com>
2021-09-05 10:10:42 +0800

    go struct && interface

--- 04_Go/ch03/t/03_struct_contain_interface.go
+++ 04_Go/ch03/t/03_struct_contain_interface.go
@@ -22,10 +22,17 @@ func (a Author) Write() {
 	fmt.Println(a.name, " Write.")
 }
 
+// 结构体 Oter 实现了接口方法 Write()，也就可以在 Author 初始化时赋值给 Writer 接口
 func (o Other) Write() {
 	fmt.Println("Other Write.")
 }
 
 func main() {
-	
+	// 方法一: Other{99} 作为 Writer 接口赋值
+	Ao := Author{ "Other", Other{99} }
+	Ao.Write()
+
+	// 方法二: 简易写法，对接口使用零值，可以完成初始化
+	Au := Author{ name: "Hawking" }
+	Au.Write()
 }
