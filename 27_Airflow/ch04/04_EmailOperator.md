2b6ea7613b03244d8f51b26ca790f771f0b439c3
libei <ytmrlimei@163.com>
2021-10-23 16:51:41 +0800

    af try emailoperator

---
+++ 27_Airflow/ch04/04_EmailOperator.md
@@ -0,0 +1,3 @@
+
+参考: [airflow.operators.email](https://airflow.apache.org/docs/apache-airflow/2.1.3/_api/airflow/operators/email/index.html#module-airflow.operators.email)
+
---
+++ 27_Airflow/ch04/t/example_email_operator.py
@@ -0,0 +1,27 @@
+
+import datetime
+
+from airflow.models.dag import DAG
+from airflow.operators.email import EmailOperator
+from airflow.utils.dates import days_ago
+
+with DAG(
+    'example_email_operator',
+    default_args={
+        'owner': 'airflow',
+        'start_date': days_ago(1),
+    },
+    schedule_interval='00 15 * * *',
+    tags=['email', 'daily'],
+) as dag:
+
+    send_email_task = EmailOperator(
+        task_id='send_email_task',
+        to='hello@email.com',
+        subject='Test Run',
+        html_content='The quick brown fox jumps over the lazy dog',
+        files=["/tmp/231.txt"],
+        dag=dag,
+    )
+
+    send_email_task
