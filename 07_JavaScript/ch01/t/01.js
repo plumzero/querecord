dfc149756951b98acdc6345c3af794cd72ceda51
libei <ytmrlimei@163.com>
2021-11-07 13:10:53 +0800

    js variable datatype

---
+++ 07_JavaScript/ch01/t/01.js
@@ -0,0 +1,17 @@
+
+"use strict";
+
+if (true) {
+    var name = 'Matt';
+    console.log(name);  // Matt
+}
+console.log(name);
+
+if (true) {
+    let age = 26;
+    console.log(age);  // Matt
+}
+// console.log(age);
+
+const age = 26;
+age = 36;
---
+++ 07_JavaScript/ch01/t/02.js
@@ -0,0 +1,10 @@
+
+"use strict";
+
+let message = "some string";
+console.log(typeof message);      // "string"
+console.log(typeof (message));    // "string"
+console.log(typeof 95);           // "number"
+
+console.log(0.1 + 0.2);           // 0.30000000000000004
+
