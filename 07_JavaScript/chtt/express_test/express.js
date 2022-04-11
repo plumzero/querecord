6885cf5928db3c95beab86a85c7fb5b69050bd0b
libei <bei.li@jingle.ai>
2022-04-12 06:25:19 +0800

    buchong

--- 07_JavaScript/chtt/express_test/express.js
+++ 07_JavaScript/chtt/express_test/express.js
@@ -6,18 +6,22 @@ const app = express();
 
 // 只解析 json，不接收其他扩展格式
 app.use(express.json());
-app.use(express.urlencoded({limit: '50mb', extended: false}));
+app.use(express.urlencoded({ limit: '1kb', extended: false }));
 
 app.use(function (req, res, next) {
     console.log('req.url:', req.url);
+    console.log('req.query:', req.query);
+    console.log('req.body:', req.body);
     console.log('Content-Type:', req.get('Content-Type'));
     console.log('Content-Length:', req.get('Content-Length'));
     next();
 });
 
+// http://192.168.2.102:3000/get?name=libei
 app.get('/get', function(req, res) {
     let message = {};
     message.msg = 'get the world';
+    console.log('req.query:', req.query);
     console.log('req.body', req.body);
     res.send(message);
 });
