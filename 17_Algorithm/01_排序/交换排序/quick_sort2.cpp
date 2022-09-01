5c19d78e9401245b81daeb1349fed84ef9e9b980
libeio <ytmrlimei@163.com>
2022-09-01 21:12:16 +0800

    quic sort

--- 17_Algorithm/01_排序/交换排序/quick_sort2.cpp
+++ 17_Algorithm/01_排序/交换排序/quick_sort2.cpp
@@ -1,5 +1,5 @@
 
-// 第一种方法
+// 第二种方法
 
 #include <stdio.h>
 
@@ -9,25 +9,33 @@
 int partition(int a[], int low, int high)
 {
     int pivot = a[low];
+    int start = low;
     while (low < high) {
-        while (low < high && pivot <= a[high])      // 将第一个元素作为枢轴元素，则必须先从后面往前比较
+        while (low < high && pivot <= a[high])  // 也必须先从后开始
             high--;
-        a[low] = a[high];
         while (low < high && pivot >= a[low])
             low++;
-        a[high] = a[low];
+        if (low < high) {
+            int tmp = a[low];
+            a[low] = a[high];
+            a[high] = tmp;
+        }
     }
-    a[low] = pivot;
+    if (start != low) {
+        int tmp = a[start];
+        a[start] = a[low];
+        a[low] = tmp;
+    }
 
     return low;
 }
 
-void quick_sort(int a[], int low, int high)
+void quick_sort(int a[], int len, int low, int high)
 {
     if (low < high) {
         int index = partition(a, low, high);
-        quick_sort(a, low, index - 1);
-        quick_sort(a, index + 1, high);
+        quick_sort(a, len, low, index - 1);
+        quick_sort(a, len, index + 1, high);
     }
 }
 
@@ -37,7 +45,8 @@ int main()
     int a[] = { 3, 2, 4, 6, 2, 8, 3, 7, 1 };
 
     int len = sizeof(a) / sizeof(int);
-    quick_sort(a, 0, len - 1);
+
+    quick_sort(a, len, 0, len - 1);
 
     for (int i = 0; i < len; i++) {
         printf("%d ", a[i]);
