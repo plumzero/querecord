ed5e6caeb2a9d837083dfa170780e02165c7ce2c
plumzero <ytmrlimei@163.com>
2023-01-17 23:27:05 +0800

    rosbag compress

--- 01_Cpp/ch11/ring/test_multiple_thread.cpp
+++ 01_Cpp/ch11/ring/test_multiple_thread.cpp
@@ -9,25 +9,23 @@
 
 int main()
 {
-    RingQueue<int> queue{102400};
+    RingQueue<int> queue{1024};
     std::atomic<size_t> cnter{0};
 
-    std::vector<std::thread> read_vec;
+    std::vector<std::thread> th_vec;
     for (int i = 0; i < SIZE; i++) {
         std::thread th = std::thread([&queue](){
-            for (int j = 0; j < 204800; j++) {
+            for (int j = 0; j < 498275; j++) {
                 queue.Put(j);
             }
         });
-        read_vec.emplace_back(std::move(th));
+        th_vec.emplace_back(std::move(th));
     }
-    std::for_each(read_vec.begin(), read_vec.end(), std::mem_fn(&std::thread::detach));
 
-    size_t arr[SIZE] = {0};
+    size_t nums[SIZE] = {0};
 
-    std::vector<std::thread> write_vec;
     for (int i = 0; i < SIZE; i++) {
-        std::thread th = std::thread([&queue, &cnter, &arr, i](){
+        std::thread th = std::thread([&queue, &cnter, &nums, i](){
             while (true) {
                 // int x;
                 // queue.Take(x);
@@ -35,20 +33,21 @@ int main()
                 std::vector<int> lst;
                 queue.Take(lst);
                 cnter += lst.size();
-                ++arr[i];
+                ++nums[i];
             }
         });
-        write_vec.emplace_back(std::move(th));
+        th_vec.emplace_back(std::move(th));
     }
-    std::for_each(write_vec.begin(), write_vec.end(), std::mem_fn(&std::thread::detach));
 
-    std::cout << "wait for 10 seconds" << std::endl;
-    std::this_thread::sleep_for(std::chrono::seconds(10));
+    std::for_each(th_vec.begin(), th_vec.end(), std::mem_fn(&std::thread::detach));
+
+    std::cout << "wait for 20 seconds" << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(20));
     std::cout << "cnter=" << cnter << std::endl;
     size_t sum = 0;
     for (size_t i = 0; i < SIZE; i++) {
-        std::cout << i << ": " << arr[i] << std::endl;
-        sum += arr[i];
+        std::cout << i << ": " << nums[i] << std::endl;
+        sum += nums[i];
     }
     std::cout << "sum=" << sum << std::endl;
     std::cout << "q.size=" << queue.Size() << std::endl;
